5bc8c1c2ed393d3ebc1dab7753e2cb3a29469dd6
John Shearer <john@ultranaut.com>
2016-02-29 14:25:25 -0500

    Tweak the newsletters index page

--- html/admin/index.php
+++ html/admin/index.php
@@ -33,9 +33,8 @@ $iNum = 0;
 $sMenuList = '';
 
 while ($oMenuRow = dbFetchObject($rMenuResult)) {
-  if (isset($sOldCategory) && ($oMenuRow->category != $sOldCategory || $sOldCategory == '')) {
-    if ($iNum%2 != 0)
-    $sMenuList .= "<td bgcolor = \"eeeeee\">&nbsp;</tD>";
+  if (!isset($sOldCategory) || ($oMenuRow->category != $sOldCategory || $sOldCategory == '')) {
+    if ($iNum%2 != 0) $sMenuList .= "<td bgcolor = \"eeeeee\">&nbsp;</tD>";
     $sMenuList .= "</tr><tr>
       <td colspan=\"2\" align=\"center\" bgcolor = \"c1c1c1\"><b>$oMenuRow->category</b></td>
       </tr><tr>";
@@ -43,24 +42,28 @@ while ($oMenuRow = dbFetchObject($rMenuResult)) {
   }
 
   // interpret $SERVER_NAME variable, if it's there in menuLink
-  if (strstr($oMenuRow->menuLink,"\$SERVER_NAME"))
-  {
+  if (strstr($oMenuRow->menuLink,"\$SERVER_NAME")) {
     $sMenuLink = ereg_replace("\\\$SERVER_NAME",$SERVER_NAME,$oMenuRow->menuLink);
   } else {
     $sMenuLink = $oMenuRow->menuLink;
   }
 
   $sMenuList .= "<td valign=\"top\" bgcolor = \"eeeeee\" width=\"50%\">
-        <ul>
-        <li><a href=\"". $sMenuLink."?iMenuId=$oMenuRow->id\"><b>$oMenuRow->menuItem</b></a> &nbsp;";
-      if ($oMenuRow->description != '') {
-          $sMenuList .= "<A href='JavaScript:void(window.open(\"menuDesc.php?iMenuId=$oMenuRow->id\", \"\", \"height=200, width=300, scrollbars=auto, resizable=yes, status=no\"));' class=header>?</a>";
-      }
-      $sMenuList .= "</li>
-        </ul></td>";
+        <ul>";
+  if ($oMenuRow->menuItem == 'Newsletters Automation') {
+    $sMenuList .= "<li><a href=\"". $sMenuLink."?iMenuId=$oMenuRow->id\"><b>$oMenuRow->menuItem</b></a> &nbsp;";
+  } else {
+    $sMenuList .= "<li><b style=\"color: #999;\">$oMenuRow->menuItem</b> &nbsp;";
+  }
+
+  /* if ($oMenuRow->description != '') { */
+  /*     $sMenuList .= "<A href='JavaScript:void(window.open(\"menuDesc.php?iMenuId=$oMenuRow->id\", \"\", \"height=200, width=300, scrollbars=auto, resizable=yes, status=no\"));' class=header>?</a>"; */
+  /* } */
+
+  $sMenuList .= "</li></ul></td>";
 
   $iNum++;
-  if ($iNum%2 == 0) {
+  if ($iNum % 2 == 0) {
     $sMenuList .= "</tr>";
   }
 
