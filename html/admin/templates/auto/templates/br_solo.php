1d7cc20814b22697ca40f01a8bb23aa02027855e
John Shearer <john@ultranaut.com>
2017-07-21 16:45:46 -0400

    Make the solo templates "responsive"

--- html/admin/templates/auto/templates/br_solo.php
+++ html/admin/templates/auto/templates/br_solo.php
@@ -3,34 +3,41 @@
 <title>Better Recipes - Special Offer</title>
 </head>
 <body bgcolor="#FFFFFF">
-<table width="640" border="0" align="center" cellpadding="0" cellspacing="0">
+
+<table border="0" align="center" cellpadding="0" cellspacing="0" style="width:100%; max-width:640px; min-width:360px;">
   <tr>
-    <td align="center"><img src="https://cdn.maropost.com/pro/uploads/account_694/70071/90ee2br_special.jpeg?rand=6ec54ee7-8370-4730-b16a-5737ff7faf42"/></td>
+    <td align="center"><img src="https://cdn.maropost.com/pro/uploads/account_694/70071/90ee2br_special.jpeg?rand=6ec54ee7-8370-4730-b16a-5737ff7faf42" alt="" width="90%"/></td>
   </tr>
-<tr><td colspan="3" align="center" valign="middle"> <font size="1" color="#999999">PAID ADVERTISEMENT</font><br />
-			</td></tr>
-	<tr>
-		<td style="padding:24px 0px; text-align:center;">
-			{{/SLOT_TEXT/}}
-		</td>
-	</tr>
-<table style="font-family:Arial, Helvetica, sans-serif; font-size:12px; margin: 0 auto;" cellpadding="0" cellspacing="0" border="0">
-      <tr>
-      <td style="margin: 0; padding-top: 5px;">
-	<br>
-	This e-mail was requested by: {to}.
-<br>
-To unsubscribe, <a href="http://unsubscribe.betterrecipes.com/?c=unsub&lid=505&jid={job.jobid}" target="_blank">click here</a>.
-<br>
-Privacy Policy: <a href="http://betterrecipes.com/privacy-policy/" target="_blank">click here</a>.
-<br>
-<P>The preceding is a paid message from a Better Recipes advertiser and does not 
-reflect the views of Better Recipes. Any questions or concerns regarding this offer should be directed to the advertiser.</P>
-June Media, Inc., 200 E. Ohio Street, Suite 300, Chicago, IL 60611
-<br>
-Copyright &copy; {{/CURRENT_YEAR/}} June Media Inc. All rights reserved. 
-	<br>
-  </td></tr></table>
+  <tr>
+    <td colspan="3" align="center" valign="middle">
+      <font size="1" color="#999999">PAID ADVERTISEMENT</font><br />
+    </td>
+  </tr>
+
+  <tr>
+    <td style="padding:24px 0px; text-align:center;">
+      {{/SLOT_TEXT/}}
+    </td>
+  </tr>
+
+  <tr>
+    <td style="padding:20px 0px; text-align:left; color:#2E2E2E; font-family:Arial, Helvetica, sans-serif; font-size:12px;">
+
+      <p>
+        This e-mail was requested by: {to}.<br>
+        To unsubscribe, <a href="http://unsubscribe.betterrecipes.com/?c=unsub&lid=505&jid={job.jobid}" target="_blank">click here</a>.<br>
+        Privacy Policy: <a href="http://betterrecipes.com/privacy-policy/" target="_blank">click here</a>.
+      </p>
+
+      <p>The preceding is a paid message from a Better Recipes advertiser and does not reflect the views of Better Recipes. Any questions or concerns regarding this offer should be directed to the advertiser.</p>
+
+      <p>
+        June Media, Inc., 200 E. Ohio Street, Suite 300, Chicago, IL 60611<br>
+        Copyright &copy; {{/CURRENT_YEAR/}} June Media Inc. All rights reserved.<br>
+      </p>
+    </td>
+  </tr>
+</table>
 
 <!-- LiveRamp tags -->
 <img border='0' hspace='0' vspace='0' width='1' height='1' src='https://ei.rlcdn.com/447946.gif?s={{contact.email_sha1}}&n=1'/>
