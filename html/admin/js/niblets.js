1d48f7df90b2d35b37e2226265a279a7065061c6
John Shearer <john@ultranaut.com>
2018-04-26 11:12:42 -0400

    Brought r4l.popularliving.com/subctr/js/ajax.js into Niblets

--- html/admin/js/niblets.js
+++ html/admin/js/niblets.js
@@ -23,7 +23,7 @@ function addImageToLibrary(key) {
 
 
         // expecting back a string here
-        response=coRegPopup.send('addImageToLibrary.php?imageurl=' + imgURL, '');
+        response=nibletsAjax.send('addImageToLibrary.php?imageurl=' + imgURL, '');
         console.info('response:', response);
 
         if (response.indexOf('maropost.s3.amazonaws.com') != -1 || response.indexOf('cdn.maropost.com') != -1) {
@@ -44,3 +44,45 @@ function addImageToLibrary(key) {
   console.log('[addImageToLibrary]', 'field is empty' );
   return true;
 }
+
+/**
+ * Brought in from http://r4l.popularliving.com/subctr/js/ajax.js
+ *   and tweaked
+ */
+var nibletsAjax = {
+  _xh: null,
+
+  init: function () {
+    var xmlHttp=false;
+    if (!xmlHttp && typeof XMLHttpRequest != 'undefined') {
+      xmlHttp = new XMLHttpRequest();
+    }
+
+    try {
+      // Mozilla / Safari
+      this._xh = new XMLHttpRequest();
+    }
+    catch (e) {
+      // Explorer
+      this._xh = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+  },
+
+  busy: function () {
+    return (this._xh.readyState && (this._xh.readyState > 4))
+  },
+
+  send: function (url, data) {
+    if (!this._xh) {
+      this.init();
+    }
+    if (!this.busy()) {
+      this._xh.open("GET", url, false);
+      this._xh.send(data);
+      if (this._xh.readyState == 4 && this._xh.status == 200) {
+        return this._xh.responseText;
+      }
+    }
+    return false;
+  }
+};
